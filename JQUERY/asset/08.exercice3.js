5dd65b923ce658fb77bb86f1d6ec9a126b2457dd
hanalina <35594531+hanalina@users.noreply.github.com>
2018-01-30 08:08:30 -0800

    travail du 30.01.2018
    4 exos jquery

--- JQUERY/asset/08.exercice3.js
+++ JQUERY/asset/08.exercice3.js
@@ -1,83 +1,165 @@
-// -- Initialisation de jQuery
-$(() => {
+// -- Initialisation de jQuery (DOM READY)
+$(function() {
 
-    // -- Ecouter a quel moment est soumis notre formulaire
-    // En JS : document.getElementById('contact').addEventListener('submit', MaFonctionAExecuter);
-    $('#contact').on('submit', e => {
+    // -- Tableau indexé d'objet Contact
+    var CollectionDeContacts = [];
 
-        // -- Neutraliser la redirection HTML5
-        e.preventDefault();
+    /* --------------------------------------------------------------
+                        DECLARATION DES FONCTIONS
+    -------------------------------------------------------------- */
 
-        // -- Supprimer les différentes erreurs
-        $('#contact .has-error').removeClass('has-error');
-        $('#contact .text-danger').remove();
+    function ajouterContact(Contact) {
 
-        // -- Déclarer les variables à vérifier
-        var nom     = $('#nom');
-        var prenom  = $('#prenom');
-        var email   = $('#email');
-        var tel     = $('#tel');
+        // -- Ajouter "Contact" dans "CollectionDeContacts"
+        CollectionDeContacts.push(Contact);
+        console.log(CollectionDeContacts);
+        
+        // -- On cache la phrase : Aucun Contact.
+        $('.aucuncontact').hide();
 
-        // -- Vérification de chaque champ
+        // -- Mise à jour du HTML
+        $(`
+            <tr>
+                <td>` + Contact.nom + `</td>
+                <td>${Contact.prenom}</td>
+                <td>${Contact.email}</td>
+                <td>${Contact.tel}</td>
+            </tr>
+        `).appendTo($('#LesContacts > tbody'));
 
-            // -- 1. Vérification du Nom
-            if(nom.val().length === 0) {
-                nom.parent().addClass('has-error');
-                $('<p class="text-danger">N\'oubliez pas de saisir votre nom</p>').appendTo(nom.parent());
-            } else {
-                nom.parent().addClass('has-success');
+        // -- Réinitialisation du Formulaire
+        reinitialisationDuFormulaire();
+
+        // -- Affiche une Notification
+        $('.alert-contact').fadeIn().delay(4000).fadeOut();
+
+    }
+
+    function reinitialisationDuFormulaire() {
+        //  -- En jQuery 
+        $('#contact').trigger('reset');
+        $('#contact').get(0).reset();
+        $('#contact .form-control').val('');
+        
+        // -- En Javascript :
+        document.getElementById('contact').reset();
+    }
+
+    function validateEmail(email){
+        var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        var valid = emailReg.test(email);
+    
+        if(!valid) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    var validateTel = tel => {
+        var telReg = new RegExp("(0|\\+33|0033)[1-9][0-9]{8}");
+        var valid = telReg.test(tel);
+    
+        if(!valid) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function unContactEstPresent(Contact) {
+
+        // -- Booleen qui indique la présence d'un contact dans ma collection
+        let estPresent = false;
+
+        // -- On parcourt le tableau à la recherche d'une correspondance
+        for(let i = 0 ; i < CollectionDeContacts.length ; i++) {
+
+            if(Contact.email === CollectionDeContacts[i].email) {
+                // -- Si une correspondance est trouvé "estPresent" passe à VRAI (true)
+                estPresent = true;
+                // -- On arrête la boucle, plus besoin de poursuivre.
+                break;
             }
 
-        // -- 2. Vérification du Prénom
+        }
+
+        // -- On retourne le booleen
+        return estPresent;
+
+    }
+
+    /* --------------------------------------------------------------
+                    TRAITEMENT DE MON FORMULAIRE
+    -------------------------------------------------------------- */
+
+    // -- Détection de la soumission de mon formulaire
+    $('#contact').on('submit', function(e) {
+
+        // -- Stopper la redirection de la page
+        e.preventDefault();
+
+        // -- Récupération des champs à vérifier
+        var prenom, nom, email, tel;
+        nom     = $('#nom');
+        prenom  = $('#prenom');
+        email   = $('#email');
+        tel     = $('#tel');
+
+        // -- Vérification des informations
+        let mesInformationsSontValides = true;
+
+        // -- Vérification du Prénom
         if(prenom.val().length === 0) {
-            prenom.parent().addClass('has-error');
-            $('<p class="text-danger">N\'oubliez pas de saisir votre prénom</p>').appendTo(prenom.parent());
-        } else {
-            prenom.parent().addClass('has-success');
+            // -- Le champ est incorrect, car il n'a pas été rempli...
+            mesInformationsSontValides = false;
         }
 
-        // -- 3. Vérification du Mail
+        // -- Vérification du Nom
+        if(nom.val().length === 0) {
+            // -- Le champ est incorrect, car il n'a pas été rempli...
+            mesInformationsSontValides = false;
+        }
+
+        // -- Vérification du Mail
         if(!validateEmail(email.val())) {
-            email.parent().addClass('has-error');
-            $('<p class="text-danger">Vérifiez votre adresse email</p>')
-                .appendTo(email.parent());
-        } else {
-            email.parent().addClass('has-success');
+            mesInformationsSontValides = false;
         }
 
-        // -- 4. Vérification du Numéro de Téléphone
+        // -- Vérification du Tel
         if(!validateTel(tel.val())) {
-            tel.parent().addClass('has-error');
-            $('<p class="text-danger">Vérifiez votre numéro de téléphone</p>')
-                .appendTo(tel.parent());
-        } else {
-            tel.parent().addClass('has-success');
+            mesInformationsSontValides = false;
         }
 
-        // -- Je vérifie si mon formulaire comporte des erreurs.
-        if($('#contact').find('.has-error').length === 0) {
+        if(mesInformationsSontValides) {
 
-            $('#contact').replaceWith(`
-                <div class="alert alert-success">
-                    Votre demande a bien été envoyée !
-                    Nous vous répondrons dans les meilleurs délais.
-                </div>
-            `);
+            // -- Tout est correct
+            let Contact = {
+                //cle       //valeur
+                nom         : nom.val(),
+                prenom      : prenom.val(),
+                email       : email.val(),
+                tel         : tel.val()
+            };
+
+            /**
+             * Si le contact est présent dans la collection, on informe l'utilisateur
+             * Sinon, on peut procéder à la suite du traitement.
+             */
+            if(unContactEstPresent(Contact)) {
+                reinitialisationDuFormulaire();
+                alert('ATTENTION\nCe contact est déjà présent !');
+            } else {
+                ajouterContact(Contact);
+            }
 
         } else {
 
-            $('#contact').prepend(`
-                <div class="alert alert-danger">
-                    Nous n'avons pas été en mesure de valider votre
-                    demande. Vérifiez vos informations.
-                </div>
-            `);
+            // -- Tous les champs n'ont pas été remplis.
+            alert('ATTENTION\nVeuillez bien remplir tous les champs.');
 
         }
 
-    });
+    })
 
-});
-
-
-// pour ajouter un nouveau contact c'est avec pop ou shift 
+})
---
+++ JQUERY/asset/09.exercice4.js
@@ -0,0 +1,18 @@
+// mettre un push pour ajouter un espace de geoloc ds le formulaire ?
+
+curl 'https://ipapi.co/8.8.8.8/json/'
+
+
+
+{
+    "ip" : "8.8.8.8", 
+    "city" : "paris", 
+    "region" : "Ile-De-France", 
+    "country" : "France", 
+    "postal" : "75004", 
+    "latitude" : 37.3845, 
+    "longitude" : -122.0881,
+    "timezone" : "France/Los_Pariss"
+    "asn" : 15169
+    "org" : "Google Inc."
+}
